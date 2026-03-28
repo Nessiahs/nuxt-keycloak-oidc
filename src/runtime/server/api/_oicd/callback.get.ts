45132e0dea4476a9fe1a038d46063e6a567cb81d
Konstantin Kroner <konstantin.kroner@gmail.com>
2026-03-28 14:28:54 +0100

    feat(auth): add OAuth callback handler and callback handler tests with edge case

---
+++ src/runtime/server/api/_oicd/callback.get.ts
@@ -0,0 +1,103 @@
+import { getKeycloakDiscovery } from '../../../utils/keycloakDiscovery'
+
+import type { H3Event } from 'h3'
+import {
+  getQuery,
+  getCookie,
+  setCookie,
+  deleteCookie,
+  sendRedirect,
+  setResponseStatus,
+  getRequestURL,
+  defineEventHandler,
+} from 'h3'
+
+import { useRuntimeConfig } from '#app'
+import type { ResolvedModuleOptions } from '../../../../types'
+import type { KeycloakTokenResponse } from '../../../../types/keycloak.types'
+
+export default defineEventHandler(async (event: H3Event) => {
+  const runtimeConfig = useRuntimeConfig()
+  const config = runtimeConfig.keycloak as ResolvedModuleOptions
+  const discovery = await getKeycloakDiscovery(config)
+  const query = getQuery(event)
+
+  const code = query.code as string
+  const state = query.state as string
+
+  const storedState = getCookie(event, 'kc_state')
+  const verifier = getCookie(event, 'kc_verifier')
+
+  if (!code || !state || !storedState || state !== storedState || !verifier) {
+    return sendRedirect(event, '/api/auth/login')
+  }
+
+  const usedCode = getCookie(event, 'kc_code_used')
+  if (usedCode === code) {
+    setResponseStatus(event, 204)
+    return
+  }
+
+  setCookie(event, 'kc_code_used', code, {
+    httpOnly: true,
+    sameSite: 'lax',
+    secure: true,
+    maxAge: 60,
+    path: '/',
+  })
+  const url = getRequestURL(event)
+
+  const redirectUri = `${url.protocol}//${url.host}/api/auth/callback`
+
+  const body = new URLSearchParams({
+    grant_type: 'authorization_code',
+    client_id: config.clientId,
+    code,
+    redirect_uri: redirectUri,
+    code_verifier: verifier,
+  })
+
+  if (config.clientSecret) {
+    body.set('client_secret', config.clientSecret)
+  }
+
+  try {
+    const token = await $fetch<KeycloakTokenResponse>(discovery.token_endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body,
+    })
+    setCookie(event, 'kc_access', token.access_token, {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: true,
+      maxAge: token.expires_in,
+      path: '/',
+    })
+
+    setCookie(event, 'kc_refresh', token.refresh_token, {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: true,
+      maxAge: token.refresh_expires_in,
+      path: '/',
+    })
+
+    deleteCookie(event, 'kc_state')
+    deleteCookie(event, 'kc_verifier')
+
+    const userRedirectUri = getCookie(event, 'redirect_to') ?? ''
+
+    deleteCookie(event, 'redirect_to')
+
+    if (!userRedirectUri.startsWith('/')) {
+      return sendRedirect(event, '/')
+    }
+
+    return sendRedirect(event, userRedirectUri)
+  } catch {
+    return sendRedirect(event, '/api/auth/login')
+  }
+})
